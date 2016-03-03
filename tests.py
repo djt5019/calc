ff3e8496f4ea27a503857f5b8cd184640bee350f
Sam Wilson <swilson@dramafever.com>
2016-03-03 11:57:43 -0500

    Add more tests for sales tax and fix related bug

--- tests.py
+++ tests.py
@@ -4,7 +4,18 @@ import calc
 
 class CalcTestCase(TestCase):
     def test_calc(self):
-        self.assertEquals(calc.sales_discount(1, 1, "CA"), 1.08)
+        tests = [
+            (1, 1, "CA", 1.04),
+            (1, 1, "ca", 1.04),
+            (1, 1, "cA", 1.04),
+            (1, 1, "NV", 1.04),
+            (1, 1, "UT", 1.02),
+            (1, 1, "TX", .97),
+        ]
+        for test in tests:
+            qty, unit_price, state, expected = test
+            actual = calc.sales_discount(qty, unit_price, state)
+            self.assertEquals(actual, expected, (test, actual))
 
 if __name__ == '__main__':
     main()
