2c693749cf70fd911256e21155c52f4bca56f88c
Sam Wilson <swilson@dramafever.com>
2016-03-03 12:02:32 -0500

    Additional testing

--- tests.py
+++ tests.py
@@ -5,12 +5,17 @@ import calc
 class CalcTestCase(TestCase):
     def test_calc(self):
         tests = [
-            (1, 1, "CA", 1.04),
+            (1, 1, "CA", 1.04), # variations on california casing
             (1, 1, "ca", 1.04),
             (1, 1, "cA", 1.04),
-            (1, 1, "NV", 1.04),
+            (1, 1, "NV", 1.04), # test a couple non-CA states
             (1, 1, "UT", 1.02),
-            (1, 1, "TX", .97),
+            (1, 1, "TX", .97),  # test a state without sales tax
+            (2, 500, "TX", 970.0),  # make sure we can do math
+            (2, 1000, "TX", 1900.0),
+            (3, 3000, "TX", 8370.0),
+            (40000.0, 1, "TX", 36000.0),
+            (50000, 2, "TX", 85000.0),
         ]
         for test in tests:
             qty, unit_price, state, expected = test
