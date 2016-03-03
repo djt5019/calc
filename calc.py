522dbe6b37926efc5ae641a3e4eeede1c69ae5b6
Sam Wilson <swilson@dramafever.com>
2016-03-03 11:29:45 -0500

    Add initial calculator library

---
+++ calc.py
@@ -0,0 +1,34 @@
+"""Internal calculator library."""
+
+def _sales_tax_rate(state):
+    """Return the sales tax amount for a given state."""
+    if "UT":
+        return .047
+    elif "CA":
+        return .075
+    elif "NV":
+        return .0685
+    elif "PA":
+        return .06
+    elif "NJ":
+        return .07
+    else:
+        return 0
+
+
+def _value_discount(base_price):
+    """Given a base_price, return the value discount."""
+    if base_price <= 1000.0:
+        return .03
+    elif base_price < 3000.0:
+        return .05
+    else:
+        return .07
+
+
+def sales_discount(qty, unit_price, state):
+    """Cacluate final price given quantity, unit_price and state."""
+    base_price = qty * unit_price
+    discount_price = base_price * (1.0 + _value_discount(base_price))
+    return discount_price * (1.0 + _sales_tax_rate(state))
+
